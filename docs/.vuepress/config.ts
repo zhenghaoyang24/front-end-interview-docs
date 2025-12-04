52cd196900fa4b6b2cde37fca6daf0183a1bf7a6
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-12-04 09:40:44 +0800

    docs: add doun2rfq

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,19 +9,19 @@
  * 特别的，请不要在两个配置文件中重复配置相同的项，当前文件的配置项会被覆盖
  */
 
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defineUserConfig } from 'vuepress'
-import { plumeTheme } from 'vuepress-theme-plume'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defineUserConfig } from "vuepress";
+import { plumeTheme } from "vuepress-theme-plume";
 
 export default defineUserConfig({
-  base: '/',
-  lang: 'zh-CN',
-  title: 'front-end-interview-docs',
-  description: 'Front-end interview questions documents.',
+  base: "/",
+  lang: "zh-CN",
+  title: "front-end-interview-docs",
+  description: "Front-end interview questions documents.",
 
   head: [
     // 配置站点图标
-    ['link', { rel: 'icon', type: 'image/svg+xml', href: 'logo.svg' }],
+    ["link", { rel: "icon", type: "image/svg+xml", href: "logo.svg" }],
   ],
 
   bundler: viteBundler(),
@@ -29,12 +29,12 @@ export default defineUserConfig({
 
   theme: plumeTheme({
     /* 添加您的部署域名, 有助于 SEO, 生成 sitemap */
-    hostname: 'https://interview.zhenghaoyang.cn',
+    hostname: "https://interview.zhenghaoyang.cn",
 
     /* 文档仓库配置，用于 editLink */
-    docsRepo: 'https://github.com/zhenghaoyang24/front-end-interview-docs',
-    docsDir: 'docs',
-    docsBranch: 'master',
+    docsRepo: "https://github.com/zhenghaoyang24/front-end-interview-docs",
+    docsDir: "docs",
+    docsBranch: "master",
 
     /* 页内信息 */
     editLink: true,
@@ -46,20 +46,20 @@ export default defineUserConfig({
      * 编译缓存，加快编译速度
      * @see https://theme-plume.vuejs.press/config/theme/#cache
      */
-    cache: 'filesystem',
+    cache: "filesystem",
 
     /**
      * 为 markdown 文件自动添加 frontmatter 配置
      * @see https://theme-plume.vuejs.press/config/theme/#autofrontmatter
      */
     autoFrontmatter: {
-      permalink: true,  // 是否生成永久链接
+      permalink: true, // 是否生成永久链接
       createTime: true, // 是否生成创建时间
-      title: true,      // 是否生成标题
+      title: true, // 是否生成标题
     },
 
     /* 本地搜索, 默认启用 */
-    search: { provider: 'local' },
+    search: { provider: "local" },
 
     /**
      * Algolia DocSearch
@@ -90,47 +90,47 @@ export default defineUserConfig({
      * markdown
      * @see https://theme-plume.vuejs.press/config/markdown/
      */
-    // markdown: {
-    //   abbr: true,         // 启用 abbr 语法  *[label]: content
-    //   annotation: true,   // 启用 annotation 语法  [+label]: content
-    //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
-    //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
-    //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
-    //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
-    //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
-    //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
-    //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
-    //   icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
-    //   table: true,        // 启用表格增强容器语法 ::: table
-    //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
-    //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
-    //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
-    //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
-    //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
-    //   demo: true,         // 启用 demo 容器  ::: demo
-    //   collapse: true,     // 启用折叠容器  ::: collapse
-    //   repl: {             // 启用 代码演示容器
-    //     go: true,         // ::: go-repl
-    //     rust: true,       // ::: rust-repl
-    //     kotlin: true,     // ::: kotlin-repl
-    //     python: true,     // ::: python-repl
-    //   },
-    //   math: {             // 启用数学公式
-    //     type: 'katex',
-    //   },
-    //   chartjs: true,      // 启用 chart.js
-    //   echarts: true,      // 启用 ECharts
-    //   mermaid: true,      // 启用 mermaid
-    //   flowchart: true,    // 启用 flowchart
-    //   image: {
-    //     figure: true,     // 启用 figure
-    //     lazyload: true,   // 启用图片懒加载
-    //     mark: true,       // 启用图片标记
-    //     size: true,       // 启用图片大小
-    //   },
-    //   include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
-    //   imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
-    // },
+    markdown: {
+      //   abbr: true,         // 启用 abbr 语法  *[label]: content
+      //   annotation: true,   // 启用 annotation 语法  [+label]: content
+      //   pdf: true,          // 启用 PDF 嵌入 @[pdf](/xxx.pdf)
+      //   caniuse: true,      // 启用 caniuse 语法  @[caniuse](feature_name)
+      //   plot: true,         // 启用隐秘文本语法 !!xxxx!!
+      //   bilibili: true,     // 启用嵌入 bilibili视频 语法 @[bilibili](bid)
+      //   youtube: true,      // 启用嵌入 youtube视频 语法 @[youtube](video_id)
+      //   artPlayer: true,    // 启用嵌入 artPlayer 本地视频 语法 @[artPlayer](url)
+      //   audioReader: true,  // 启用嵌入音频朗读功能 语法 @[audioReader](url)
+      //   icon: { provider: 'iconify' },        // 启用内置图标语法  ::icon-name::
+      //   table: true,        // 启用表格增强容器语法 ::: table
+      //   codepen: true,      // 启用嵌入 codepen 语法 @[codepen](user/slash)
+      //   replit: true,       // 启用嵌入 replit 语法 @[replit](user/repl-name)
+      //   codeSandbox: true,  // 启用嵌入 codeSandbox 语法 @[codeSandbox](id)
+      //   jsfiddle: true,     // 启用嵌入 jsfiddle 语法 @[jsfiddle](user/id)
+      //   npmTo: true,        // 启用 npm-to 容器  ::: npm-to
+      demo: true, // 启用 demo 容器  ::: demo
+      //   collapse: true,     // 启用折叠容器  ::: collapse
+      //   repl: {             // 启用 代码演示容器
+      //     go: true,         // ::: go-repl
+      //     rust: true,       // ::: rust-repl
+      //     kotlin: true,     // ::: kotlin-repl
+      //     python: true,     // ::: python-repl
+      //   },
+      //   math: {             // 启用数学公式
+      //     type: 'katex',
+      //   },
+      //   chartjs: true,      // 启用 chart.js
+      //   echarts: true,      // 启用 ECharts
+      //   mermaid: true,      // 启用 mermaid
+      //   flowchart: true,    // 启用 flowchart
+      //   image: {
+      //     figure: true,     // 启用 figure
+          // lazyload: true,   // 启用图片懒加载
+      //     mark: true,       // 启用图片标记
+      //     size: true,       // 启用图片大小
+      //   },
+      //   include: true,      // 在 Markdown 文件中导入其他 markdown 文件内容
+        imageSize: 'local', // 启用 自动填充 图片宽高属性，避免页面抖动
+    },
 
     /**
      * 水印
@@ -174,4 +174,4 @@ export default defineUserConfig({
     //   locale: '/',    // 默认仅为主语言生成 llms 友好内容
     // }
   }),
-})
+});
